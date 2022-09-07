d574637008d5145b1ce3923b605bbdfcfc54c536
botong516 <bzhang16@sas.upenn.edu>
2022-09-07 12:58:29 -0400

    nav bar

---
+++ movie-search-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import { BrowserRouter as Router} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Navbar/>
+    </Router>
+  );
+}
+
+export default App;
